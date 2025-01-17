c94a0b9e6876319d6ee99e62a6bbe74aeea140f0
MisterK123 <kirin@twosmallpeople.co.uk>
2025-01-17 19:03:40 +0000

    Fixed image sizing

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 const About = () => {
     return (
-        <section className="c-space my-20">
+        <section className="c-space my-10">
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/kirinpfp2.png" alt = "grid1" className="w-full sm-h-[276] h-fit object-contain"/>
+                        <img src="/assets/kirinpfp5.PNG" alt = "grid1" className="w-full sm-h-[276] h-fit object-contain"/>
+                        <div>
+                            <p className="grid-headtext">Hi, I&#39;m Kirin</p>
+                            <p className="grid-subtext">A Y13 student currently studying Computer Science, Maths, and Physics</p>
+                        </div>
                     </div>
+
                 </div>
 
             </div>
